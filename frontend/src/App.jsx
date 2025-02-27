9d694af2709c3c372bc7d6ce114c6fa79d6d5ef1
Tejas Suthar <tejact2007@gmail.com>
2025-02-27 19:49:22 +0530

    add ui components, setup tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionPage from "./pages/TransactionPage";
+import Header from "./components/ui/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const authUser = true;
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signUp" element={<SignUpPage />}></Route>
+        <Route path="/transaction/:id" element={<TransactionPage />}></Route>
+        {/* <Route path="/*" element={<NotFoundPage />}></Route> */}
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import InputField from "../components/InputField";
+import RadioButton from "../components/RadioButton";
+
+const SignUpPage = () => {
+  const [signUpData, setSignUpData] = useState({
+    name: "",
+    username: "",
+    password: "",
+    gender: "",
+  });
+
+  const handleChange = () => {};
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex rounded-lg overflow-hidden z-50 bg-gray-300">
+        <div className="w-full bg-gray-100 min-w-80 sm:min-w-96 flex items-center justify-center">
+          <div className="max-w-md w-full p-6">
+            <h1 className="text-3xl font-semibold text-black mb-6 text-center">
+              Sign Up
+            </h1>
+            <h1 className="text-sm font-semibold mb-6 text-gray-500 text-center">
+              Join to keep track of your expenses
+            </h1>
+            <form className="space-y-4" onSubmit={handleSubmit}>
+              <InputField
+                label="Full Name"
+                id="name"
+                name="name"
+                value={signUpData.name}
+                onChange={handleChange}
+              />
+
+              <InputField
+                label="Username"
+                id="username"
+                name="username"
+                value={signUpData.username}
+                onChange={handleChange}
+              />
+
+              <InputField
+                label="Password"
+                id="password"
+                name="password"
+                type="password"
+                value={signUpData.password}
+                onChange={handleChange}
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
